2fcb1c9c648accbda816322cbe5dc816b1a3364d
collins <collinschuwa@gmail.com>
2022-03-11 03:16:13 +0100

    Updates

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
-#include <stdio.h>  
+#include <stdio.h>
 
-int main()
+int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 
-    return 0;
+return (0);
 }
