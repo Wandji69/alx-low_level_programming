6764a3bfc67c80d149f568165cf95dc11afe58e2
collins <collinschuwa@gmail.com>
2022-03-13 15:46:01 +0100

    Number

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+for (int i = 0; i < 10 ; i++)
+{
+putchar((i % 10) + '0');
+}
+putchar('\n');
+
+return (0);
+}
