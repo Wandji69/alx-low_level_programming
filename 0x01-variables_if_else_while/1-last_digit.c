2205b85891cc3828e43d859ca0514db6b8d97829
collins <collinschuwa@gmail.com>
2022-03-14 09:48:22 +0100

    fixing code style

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,9 +3,6 @@
 /* more headers goes there */
 #include <stdio.h>
 
-/* Last_Digit takes a value num */
-int Last_Digit(int num);
-
 /**
  * main - Entry point
  *
@@ -20,9 +17,13 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
 
+/* Last_Digit takes a value num */
+int num;
+int Last_Digit(num);
+
+/* call the Last_Digit function to retun the last the number */
 int last;
 last = Last_Digit(n);
-/* call the Last_Digit function to retun the last the number */
 if (last > 5)
 {
 	printf("Last digit of %d is %d and is greater than 5\n", n, last);
@@ -40,7 +41,7 @@ return (0);
 }
 
 /* Last_Digit Function of type int */
-int Last_Digit(int num)
+int Last_Digit(num)
 {
 	/* Return  int */
 	return (num % 10);
