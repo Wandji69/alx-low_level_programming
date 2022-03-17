75a26ecbe7db9ddd2900beb9df836fc62050cc3a
collins <collinschuwa@gmail.com>
2022-03-17 01:57:06 +0100

    task-0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *
+ * Main - Entry point
+ *
+ * Retrun Always 0 (Success)
+ */
+int main(void)
+{
+    char text[10] = "_putchar";
+
+    int i;
+
+    for (i = 0; i < 5; i++)
+    {
+        _putchar(text[i]);
+    }
+    _putchar('\n');
+
+    return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+## Workiing with variables, if..else Statement.
+
+This repo contains code that is to test my ability to work with if...else statement and thinking
