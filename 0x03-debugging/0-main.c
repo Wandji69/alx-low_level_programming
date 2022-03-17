2a416eaf8b751e44746595210ef115e65085bee9
collins <collinschuwa@gmail.com>
2022-03-17 18:41:00 +0100

    checking for positive or negative values

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * @brief testing for positive or negative number
+ * 
+ * Return Always return 0 on success
+ */
+int main(void)
+{
+int num;
+num = 0;
+positive_or_negative(num);
+
+return (0);
+}
