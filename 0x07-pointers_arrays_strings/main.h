a1627645bb51d16587b50a35302e62cbbbe7e2bc
collins <collinschuwa@gmail.com>
2022-03-29 00:51:18 +0100

    Task 2, Task 3 & Task 4

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,5 +3,8 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_memset(char *s, char b, unsigned int n);
+char *_strpbrk(char *s, char *accept);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
 
 #endif
