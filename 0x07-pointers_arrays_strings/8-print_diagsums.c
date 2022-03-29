49c7f961c9c3c3f0cc433d4069cc23b453ad6536
collins <collinschuwa@gmail.com>
2022-03-29 01:32:36 +0100

    update

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagsums - Entry point
+ * @a: input
+ * @size: input
+ * Return: Always 0 (Success)
+ */
+void print_diagsums(int *a, int size)
+{
+int i, n, total1 = 0, total2 = 0;
+
+for (i = 0; i <= (size * size); i = i + size + 1)
+{
+total1 = total1 + a[i];
+
+for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
+{
+total2 = total2 + a[n];
+}
+}
+
+_putchar(total1, total2);
+}
